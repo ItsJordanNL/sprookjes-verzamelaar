92e81e05c915e2490be23e917dc7db10d571f4c8
Koevoets,Jordan J.J.R.A <j.koevoets@student.fontys.nl>
2024-04-05 19:31:41 +0200

    Style - Navbar kleuren veranderen

--- lib/navigation_bar.dart
+++ lib/navigation_bar.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import './screens/verzameling.dart'; // Importeer de verzameling.dart
-import 'screens/instellingen.dart';
+import './screens/verzameling.dart';
+import './screens/instellingen.dart';
 import './screens/kleurplaten.dart';
-
-/// Importeer de settings.dart
+import './components/constants.dart';
 
 class BottomNavigation extends StatefulWidget {
   const BottomNavigation({super.key});
@@ -21,23 +20,44 @@ class _BottomNavigationState extends State<BottomNavigation> {
       body: IndexedStack(
         index: currentPageIndex,
         children: const [
-          Verzameling(), // Widget voor de "Home" bestemming
-          Kleurplaten(), // Widget voor de "Profile" bestemming
-          Instellingen(), // Widget voor de "Settings" bestemming
+          Verzameling(), // Widget for the "Home" destination
+          Kleurplaten(), // Widget for the "Profile" destination
+          Instellingen(), // Widget for the "Settings" destination
         ],
       ),
-      bottomNavigationBar: NavigationBar(
-        destinations: const [
-          NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
-          NavigationDestination(icon: Icon(Icons.person), label: 'Kleurplaten'),
-          NavigationDestination(icon: Icon(Icons.settings), label: 'Settings'),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.grid_on_outlined, color: textcolor),
+            activeIcon: Icon(Icons.grid_on,
+                color: textcolor), // Icon filled when active
+            label: 'Verzameling',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.palette_outlined, color: textcolor),
+            activeIcon: Icon(Icons.palette,
+                color: textcolor), // Icon filled when active
+            label: 'Kleurplaten',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings_outlined, color: textcolor),
+            activeIcon: Icon(Icons.settings,
+                color: textcolor), // Icon filled when active
+            label: 'Instellingen',
+          ),
         ],
-        selectedIndex: currentPageIndex,
-        onDestinationSelected: (int index) {
+        currentIndex: currentPageIndex,
+        onTap: (int index) {
           setState(() {
             currentPageIndex = index;
           });
         },
+        backgroundColor: red,
+        unselectedItemColor: textcolor
+            .withOpacity(0.75), // Set unselected item color to a lighter shade
+        selectedItemColor: textcolor, // Set selected item color to white
+        unselectedIconTheme: IconThemeData(
+        color: textcolor.withOpacity(0.75)), // Set the theme for selected icons
       ),
     );
   }
