4fb4881cc0fab9fa5bb88122e455a0e1a90b3244
Verwegen,Daniell,D <d.verwegen@student.fontys.nl>
2024-04-05 12:07:46 +0200

    build - flutter splash screen package

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 import './screens/test.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MyApp());
+
+    FlutterNativeSplash.remove();
 }
+
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
