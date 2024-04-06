f8e2d693c075b4f66c85f03623c4120194e93692
Koevoets,Jordan J.J.R.A <j.koevoets@student.fontys.nl>
2024-04-07 00:38:53 +0200

    Build - Item grid maken en afbeeldingen van sprookjes toevoegen

--- lib/screens/verzameling.dart
+++ lib/screens/verzameling.dart
@@ -2,42 +2,53 @@ import 'package:flutter/material.dart';
 import '../components/constants.dart';
 
 class Verzameling extends StatefulWidget {
-  const Verzameling({super.key});
+  const Verzameling({Key? key}) : super(key: key);
 
   @override
-  State<Verzameling> createState() => _HomeState();
+  State<Verzameling> createState() => _VerzamelingState();
 }
 
-class _HomeState extends State<Verzameling> {
+class _VerzamelingState extends State<Verzameling> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          appBar: PreferredSize(
-              preferredSize:
-                  const Size.fromHeight(85), // here the desired height
-              child: AppBar(
-                toolbarHeight: 85, // Set this height
-                centerTitle: true,
-                title: Container(
-                  margin: const EdgeInsets.only(
-                      top: 40), // Adjust the bottom margin as needed
-                  child: const Text(
-                    'Verzameling',
-                    style:
-                        TextStyle(fontFamily: 'Pretaporter Slab', fontSize: 30),
-                  ),
-                ),
-                backgroundColor: darkred,
-                foregroundColor: textcolor,
-              )),
-          body: const SingleChildScrollView(
-            child: Text(
-              " Hello world!",
-              style: TextStyle(fontSize: 24),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(85), // here the desired height
+          child: AppBar(
+            toolbarHeight: 85, // Set this height
+            centerTitle: true,
+            title: Container(
+              margin: const EdgeInsets.only(top: 40), // Adjust the bottom margin as needed
+              child: const Text(
+                'Verzameling',
+                style: TextStyle(fontFamily: 'Pretaporter Slab', fontSize: 30),
+              ),
+            ),
+            backgroundColor: darkred,
+            foregroundColor: textcolor,
+          ),
+        ),
+        body: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/achtergrond.jpg'),
+              fit: BoxFit.cover,
             ),
           ),
-        ));
+          child: GridView.count(
+            crossAxisCount: 3,
+            children: List.generate(30, (index) {
+              // Assuming you have image assets named 'sprookje1.jpg', 'sprookje2.jpg', ..., 'sprookje30.jpg' in a folder named 'sprookjes'
+              return Image.asset(
+                'assets/images/sprookjes/sprookje${index + 1}.jpg',
+                fit: BoxFit.cover,
+              );
+            }),
+          ),
+        ),
+      ),
+    );
   }
 }
