ebb25599da56a24b8f06c57e22052e45e0791ef6
Koevoets,Jordan J.J.R.A <j.koevoets@student.fontys.nl>
2024-04-06 23:49:29 +0200

    Style - Standaard lettertype naar Roboto

--- lib/main.dart
+++ lib/main.dart
@@ -16,9 +16,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: ThemeData(fontFamily: 'Roboto'),
       debugShowCheckedModeBanner: false,
-      home: Home(),
+      home: const Home(),
     );
   }
 }
