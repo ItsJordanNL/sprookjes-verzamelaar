062140608b806cd6f6610aa460f35a56b53a18fc
Koevoets,Jordan J.J.R.A <j.koevoets@student.fontys.nl>
2024-04-07 01:00:20 +0200

    Style - Grid met grijze overlays die weg gaan op klik

--- lib/screens/verzameling.dart
+++ lib/screens/verzameling.dart
@@ -9,18 +9,20 @@ class Verzameling extends StatefulWidget {
 }
 
 class _VerzamelingState extends State<Verzameling> {
+  List<bool> imageClickedList = List.generate(30, (index) => false);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: PreferredSize(
-          preferredSize: const Size.fromHeight(85), // here the desired height
+          preferredSize: const Size.fromHeight(85),
           child: AppBar(
-            toolbarHeight: 85, // Set this height
+            toolbarHeight: 85,
             centerTitle: true,
             title: Container(
-              margin: const EdgeInsets.only(top: 40), // Adjust the bottom margin as needed
+              margin: const EdgeInsets.only(top: 40),
               child: const Text(
                 'Verzameling',
                 style: TextStyle(fontFamily: 'Pretaporter Slab', fontSize: 30),
@@ -37,15 +39,46 @@ class _VerzamelingState extends State<Verzameling> {
               fit: BoxFit.cover,
             ),
           ),
-          child: GridView.count(
-            crossAxisCount: 3,
-            children: List.generate(30, (index) {
-              // Assuming you have image assets named 'sprookje1.jpg', 'sprookje2.jpg', ..., 'sprookje30.jpg' in a folder named 'sprookjes'
-              return Image.asset(
-                'assets/images/sprookjes/sprookje${index + 1}.jpg',
-                fit: BoxFit.cover,
-              );
-            }),
+          child: Padding(
+            padding: const EdgeInsets.all(15),
+            child: GridView.count(
+              crossAxisCount: 3,
+              mainAxisSpacing: 20,
+              crossAxisSpacing: 20,
+              children: List.generate(30, (index) {
+                // Assuming you have image assets named 'sprookje1.jpg', 'sprookje2.jpg', ..., 'sprookje30.jpg' in a folder named 'sprookjes'
+                return GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      imageClickedList[index] = !imageClickedList[index];
+                    });
+                  },
+                  child: Container(
+                    margin: const EdgeInsets.all(2),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(99999),
+                      border: Border.all(color: textcolor, width: 5),
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(99999),
+                      child: Stack(
+                        fit: StackFit.expand,
+                        children: [
+                          Image.asset(
+                            'assets/images/sprookjes/sprookje${index + 1}.jpg',
+                            fit: BoxFit.cover,
+                          ),
+                          if (!imageClickedList[index])
+                            Container(
+                              color: Colors.grey.withOpacity(0.8), // Gray overlay
+                            ),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              }),
+            ),
           ),
         ),
       ),
