3169035a6c7e5c21ed01923ee4f61f2ac7c6c980
Koevoets,Jordan J.J.R.A <j.koevoets@student.fontys.nl>
2024-04-09 15:23:02 +0200

    Style - Aanpassen achtergrond en kleur appbar en navbar

--- lib/components/constants.dart
+++ lib/components/constants.dart
@@ -1,10 +1,10 @@
 import "package:flutter/material.dart";
 
 const Color darkblue =  Color.fromARGB(255, 0, 1, 72);
-const Color lightblue = Color.fromARGB(255, 5,160,183);
-const Color green = Color.fromARGB(255,1,155,85);
-const Color primary = Color.fromARGB(255,172,26,42);
-const Color red = Color.fromARGB(255,183,42,58);
+const Color lightblue = Color.fromARGB(255, 5, 160, 183);
+const Color green = Color.fromARGB(255, 1, 155, 85);
+const Color primary = Color.fromARGB(255, 0, 1, 72);
+const Color red = Color.fromARGB(255, 183, 42, 58);
 
 const Color textcolor = Colors.white;
 const Color textcolordark = Colors.black;
