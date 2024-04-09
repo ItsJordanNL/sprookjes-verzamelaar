4d21eb0f2a3741008b53f5368e0e0561e77afc93
Verwegen,Daniell,D <d.verwegen@student.fontys.nl>
2024-04-09 15:22:50 +0200

    fix - appbar background transparent

--- lib/screens/verhaal.dart
+++ lib/screens/verhaal.dart
@@ -17,14 +17,31 @@ class Verhaal extends StatelessWidget {
   Widget build(BuildContext context) {
     _getInitialInfo();
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: appBar(),
-      body: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
-       _imageSection(),
-       const SizedBox(height:20),
-       
+      body: Stack(
+        children: [
+          _backgroundImage(),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const SizedBox(height: 100),
+              _imageSection(),
+              const SizedBox(height: 20),
+              // Voeg hier andere widgets toe voor de inhoud van de pagina
+            ],
+          ),
+        ],
+      ),
+    );
+  }
 
-      
-      ],)
+  Widget _backgroundImage() {
+    return Positioned.fill(
+      child: Image.asset(
+        'assets/images/paperbackground.jpg', // Vervang 'background_image.jpg' door het pad naar jouw achtergrondafbeelding
+        fit: BoxFit.cover,
+      ),
     );
   }
 
@@ -32,11 +49,11 @@ class Verhaal extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(20),
       child: ClipRRect(
-        borderRadius: BorderRadius.circular(10), // Pas hier de gewenste border-radius-waarde aan
+        borderRadius: BorderRadius.circular(10),
         child: Image.asset(
           sprookje[selectedIndex].image,
-          width: double.infinity, 
-          height:200,
+          width: double.infinity,
+          height: 200,
           fit: BoxFit.cover,
         ),
       ),
@@ -45,6 +62,8 @@ class Verhaal extends StatelessWidget {
 
   AppBar appBar() {
     return AppBar(
+      backgroundColor: Colors.transparent, // Doorzichtige achtergrondkleur
+      elevation: 0, // Verwijder de schaduw
       title: Text(
         sprookje.length > selectedIndex
             ? sprookje[selectedIndex].title
